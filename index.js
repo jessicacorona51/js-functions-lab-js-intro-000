d5658b617ece2782e563c36347cc86429e7ab4f6
Jessica Corona <jessicacorona51@yahoo.com>
2020-05-26 22:59:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function happyHolidays() {
   return ("Happy holidays!")
 }
 function happyHolidaysTo() {
-  return `Happy holidays, ${name}!`
+  console.log(`Happy holidays, ${name}`)
 }
 function happyCustomHolidayTo() {
   return `Happy ${holiday}, ${name}!`
